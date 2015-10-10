754021abff66383ff31ae7683c5076687a3fab6f
sebastian cortes <seba.cortes1989@gmail.com>
2015-10-10 11:36:21 -0300

    pruebas

--- public/index.php
+++ public/index.php
@@ -46,6 +46,8 @@ $app = require_once __DIR__.'/../bootstrap/app.php';
 | and wonderful application we have prepared for them.
 |
 */
+echo 0;
+
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
